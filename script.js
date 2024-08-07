4917cef823d27086f63a5bdc23b6acbf8d11f578
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-08-07 19:52:45 +0300

    update

--- script.js
+++ script.js
@@ -1,19 +1,31 @@
+const _card = document.getElementById('card');
 const _name = document.getElementById('name');
 const _img = document.getElementById("img");
 const _hp = document.getElementById('hp');
 const _def = document.getElementById('defense');
 const _atk = document.getElementById('attack');
+let _data;
+
+_card.style.visibility = 'hidden';
 
 function updateStats(name, imgSrc, stats){
-    _name.textContent = name;
-    _img.src = imgSrc;
-    _hp.textContent = `HP:${stats[0].base_stat}`;
-    _def.textContent = `DEFENSE:${stats[2].base_stat}`;
-    _atk.textContent = `ATTACK:${stats[1].base_stat}`;
+    _card.style.visibility = "visible";
+    _name.textContent = _data.name;
+    _img.src = _data.sprites.front_default;
+    _hp.textContent = `HP:${_data.stats[0].base_stat}`;
+    _def.textContent = `DEFENSE:${_data.stats[2].base_stat}`;
+    _atk.textContent = `ATTACK:${_data.stats[1].base_stat}`;
+}
+
+function turnAround(){
+    if(_img.src === _data.sprites.front_default)
+        _img.src = _data.sprites.back_default
+    else   
+        _img.src = _data.sprites.front_default;
 }
 
 function fetchData(){
-    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+    const pokemonName = document.getElementById("pokemonName").value.trim().toLowerCase();
     fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
         .then(response => {
             if(!response.ok){
@@ -24,8 +36,8 @@ function fetchData(){
         })
         .then(data => {
             console.log(data);
-
-            updateStats(data.name, data.sprites.front_default, data.stats);
+            _data = data;
+            updateStats();
         })
         .catch(error => console.log(error));
 }
