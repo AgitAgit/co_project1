f2ba821b29a5dca3c116c6a392c35c2d7db5e9d4
YanivRidel <117529227+Yanivridel@users.noreply.github.com>
2024-08-07 15:58:36 +0300

    pokemon backend

--- script.js
+++ script.js
@@ -1,2 +1,23 @@
 
 
+
+function fetchData(){
+    const pokemonName = document.getElementById("pokemonName").value.toLowerCase();
+    fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonName}`)
+        .then(response => {
+            if(!response.ok){
+                throw new Error("Could not fetch resource")
+            }
+            
+            return response.json();
+        })
+        .then(data => {
+            console.log(data)
+            const pokemonSprite = data.sprites.front_default;
+            const imgElement = document.getElementById("pokemonSprite");
+            
+            imgElement.src = pokemonSprite;
+            imgElement.style.display = "block";
+        })
+        .catch(error => console.log(error));
+}
